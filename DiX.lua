13787195e49eaf60b3df669708b573110677bce3
DoctorBloxxa <DoctorBloxxa@proton.me>
2024-07-02 21:50:11 +0200

    Split the spritesheet
    Modularity cuh

--- DiX.lua
+++ DiX.lua
@@ -8,8 +8,13 @@ local SelectionInterrupt = Instance.new("BindableEvent")
 local RunService = game:GetService("RunService")
 local Storage = RunService:IsStudio() and game or game.CoreGui
 local HttpService = game:GetService("HttpService")
-local LibraryURL = "https://" --- This should be the url to the raw code of the library
-local Instance = (RunService:IsStudio() and require(script:WaitForChild("Instance"))) or HttpService:JSONDecode(game:HttpGet(LibraryURL, true))
+
+local LibraryURL = "https://raw.githubusercontent.com/DoctorBloxxa/DiX/main/Almanac.lua" --- This should be the url to the raw code of the library
+local SpriteData = "https://raw.githubusercontent.com/DoctorBloxxa/DiX/main/Spritesheet.lua" --- This should be the url to the raw code of the library
+
+local Instance 		= (RunService:IsStudio() and require(script:WaitForChild("Instance"))) or HttpService:JSONDecode(game:HttpGet(LibraryURL, true))
+local ResolveTile 	= (RunService:IsStudio() and require(script:WaitForChild("Decals"))) or HttpService:JSONDecode(game:HttpGet(LibraryURL, true))
+
 local HoverThing = Instance.new("Highlight", Storage)
 local Holobolo6Billion = RaycastParams.new()
 
@@ -30,120 +35,6 @@ Holobolo6Billion.IgnoreWater = true
 
 HoverThing.OutlineColor = Color3.fromRGB(178,229,255)
 HoverThing.FillTransparency = 1
-
-local ResolveTile = {
-	IntValue				=	4,
-	NunberValue				=	4,
-	Vector3Value			=	4,
-	BoolValue				=	4,
-	StringValue				=	4,
-
-	Animation				=	60,
-
-	Fire					=	61,
-	Smoke					=	59,
-	ParticleEmitter			=	80,
-	Sparkles				=	42,
-
-	Terrain					=	65,
-	Part					=	1,
-	BasePart				=	1,
-
-	SpawnLocation			=	25,
-
-	Union					=	73,
-	NegateOperation			=	72,
-	UnionOperation			=	73,
-	MeshPart				=	73,
-
-	Decal					=	7,
-
-	Mesh					=	8,
-	SpecialMesh				=	8,
-	BlockMesh				=	8,
-	CharacterMesh			=	8,
-	WrapTarget				=	8,
-
-	Model					= 	2,
-	Folder					=	77,
-
-	Camera					=	5,
-	Humanoid				=	9,
-
-	Texture					= 	10,
-	SurfaceAppearance		= 	10,
-
-	Sound					=	11,
-	SoundService			=	31,
-
-	Player					=	12,
-
-	Teams					=	23,
-
-	Lighting				=	13,
-	Players					=	79,
-	ServerScriptService		=	71,
-	ReplicatedStorage		=	70,
-	ReplicatedFirst			=	70,
-	ServerStorage			=	69,
-
-	PointLight				=	13,
-	SpotLight				=	13,
-	SurfaceLight			=	13,
-
-	Sky						=	28,
-	ColorCorrectionEffect	=	38,
-	DepthOfFieldEffect		=	38,
-	BlurEffect				=	38,
-	SunRaysEffect			=	38,
-
-	Highlight				=	133,
-
-	Workspace				=	19,
-
-	Script					=	6,
-	LocalScript				=	18,
-	ModuleScript			=	76,
-
-	Tool					=	17,
-	HopperBin				=	22,
-
-	Explosion				=	36,
-
-	Hat						=	45,
-	Pants					=	44,
-	Shirt					=	43,
-	ShirtGraphic			=	40,
-	Accessory				=	32,
-
-	Weld					=	34,
-	Snap					=	34,
-
-	Motor6D					=	106,
-	Attachment				=	81,
-	WeldConstraint			=	94,
-
-	Seat					=	35,
-
-	ClickDetector			=	41,
-	TouchInterest			=	37,
-	ForceField				=	37,
-
-	RemoteFunction			=	74,
-	RemoteEvent				=	75,
-
-	BindableEvent			=	67,
-	BindableFunction		=	66,
-
-	ImageButton				=	52,
-	TextButton				=	51,
-	TextBox					=	51,
-	TextLabel				=	50,
-	ImageLabel				=	49,
-	Frame					=	48,
-	ScreenGui				=	47,
-	StarterGui				=	46,
-}
 
 local ReadBack = {
 
